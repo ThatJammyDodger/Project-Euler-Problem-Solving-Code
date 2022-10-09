288c1623f845c4de458e1f891140dfbb2577e572
ThatJammyDodger <ambadkarj123@gmail.com>
2022-10-09 22:24:50 +0100

    no. 16 done

---
+++ Problem 16/Program.cs
@@ -0,0 +1,26 @@
+﻿//  2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+//  What is the sum of the digits of the number 2^1000?
+
+using System.Diagnostics;
+using System.Numerics;
+Stopwatch sw = Stopwatch.StartNew();
+
+BigInteger number = 1;
+
+for (int i = 0; i < 1000; i++)
+{
+    number = number * 2;
+}
+
+string num = number.ToString();
+
+int digit_sum = 0;
+foreach(char c in num)
+{
+    digit_sum += Int32.Parse(c.ToString());
+}
+
+Console.WriteLine($"Digit sum of 2^1000 is {digit_sum}");
+sw.Stop();
+Console.WriteLine($"Elapsed time: {sw.Elapsed.TotalMilliseconds} ms");

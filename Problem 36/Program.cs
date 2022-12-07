44ef01e9dd85949aae479dbc9c9b09b03fed7d26
ThatJammyDodger <ambadkarj123@gmail.com>
2022-12-07 19:15:56 +0000

    #36 solved

--- Problem 36/Program.cs
+++ Problem 36/Program.cs
@@ -7,12 +7,36 @@
 using System.Diagnostics;
 Stopwatch sw = Stopwatch.StartNew();
 
+List<int> dpal = new();
 
-
-bool CheckPalindrome(string input)
+bool IsPalindrome(string input)
 {
-    
+    char[] chars = input.ToCharArray();
+    chars = chars.Reverse().ToArray();
+    string newstr = new string(chars);
+    return newstr == input;
 }
+
+for (int i = 1; i < 1000000; i++)
+{
+    if (IsPalindrome(i.ToString()))
+        dpal.Add(i);
+}
+
+List<int> dbpal = new();
+foreach (var num in dpal)
+{
+    if (IsPalindrome(Convert.ToString(num, 2)))
+        dbpal.Add(num);
+}
+
+int sum = 0;
+foreach (int number in dbpal)
+{
+    sum += number;
+}
+
+Console.WriteLine("The sum of palindromes under one million in both base-10 and base-2 is {0}", sum);
 
 sw.Stop();
 Console.WriteLine("Elapsed time: {0} ms", sw.Elapsed.TotalMilliseconds);
